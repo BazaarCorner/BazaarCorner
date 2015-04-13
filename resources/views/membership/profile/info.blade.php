47bcc45a1414651912d7349b6d9b16d7605474ac
gabbydgab <gamba@gabbydgab.com>
2015-04-13 14:54:51 +0800

    temporarily disabled social links

--- resources/views/membership/profile/info.blade.php
+++ resources/views/membership/profile/info.blade.php
@@ -2,11 +2,11 @@
     <div class="widget-body">
         <div class="row">
             <div class="col-md-12 text-nowrap">
-                <h4>{{$member['last_name']}}, {{$member['first_name']}}</h4>
+                <h4 class="text-capitalize">{{$member['last_name']}}, {{$member['first_name']}}</h4>
                 <span class="text-muted"><small>{{$member['address']}}</small></span>
             </div>
         </div>
-        <div class="row-fluid">
+<!--        <div class="row-fluid">
             <p>Follow me on:</p>
             <a href="{{url($member['facebook'])}}" target="_blank">
                 <i class="fa fa-facebook-square fa-2x tooltips" data-placement="bottom" data-toogle="tooltip" title="facebook">
@@ -15,6 +15,6 @@
             <a href="{{url($member['twitter'])}}" target="_blank"> 
                 <i class="fa fa-twitter-square fa-2x tooltips" data-placement="bottom" data-toogle="tooltip" title="twitter"></i>
             </a>
-        </div>
+        </div>-->
     </div>
 </div>

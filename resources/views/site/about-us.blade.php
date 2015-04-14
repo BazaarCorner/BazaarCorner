76f5a7894609c86a0aee399c21f1bfb544339153
gabbydgab <gamba@gabbydgab.com>
2015-04-15 07:47:40 +0800

    added contact information

--- resources/views/site/about-us.blade.php
+++ resources/views/site/about-us.blade.php
@@ -34,27 +34,66 @@
             e-commerce in world and making it more socialize and easy to use. 
         </p>
     </section>
-<!--    <section id="contact">
-        <h3>Contact Us</h3>
-        {!! Form::open() !!}
-            <div class="form-group">
-                {!! Form::text('') !!}
-            </div>
-            <div class="form-group">
-                {!! Form::textarea('') !!}
-            </div>
-        {!! Form::close() !!}
-    </section>-->
+    <section id="contact-information">
+        <h3>Contact Information</h3>
+        <div class="col-md-6">
+            <h4>
+                <a href="mailto:info@bazaarcorner.com">
+                    Support Team
+                </a>
+            </h4>
+            <p>Get in touch with our 24/7 support team.</p>
+            <br>
+            <h4>
+                <a href="mailto:info@bazaarcorner.com">
+                    Development Team
+                </a>                
+            </h4>
+            <p>Concerns about the security or any site functionality.</p>
+            <br>
+            <h4>
+                <a href="mailto:dennis@bazaarcorner.com">
+                    Legal Department
+                </a>                
+            </h4>
+            <p>Pertaining to Intellectual Property issues.</p>
+        </div>
+        <div class="col-md-6">
+            <h4>
+                <a href="mailto:merchant@bazaarcorner.com">
+                    Partnership
+                </a>     
+            </h4>
+            <p>
+                You want to be featured or to be one of our partners. We're
+                happy to serve you.
+            </p>
+            <br>
+            <h4>
+                <a href="mailto:press@bazaarcorner.com">
+                    PR and Media Team
+                </a>                
+            </h4>
+            <p>
+                You can send letters or creative articles with our editorial team.
+            </p>
+        </div>
+    </section>
 </div>
 <div class="col-md-3">
     <div class="affix">
-        <h4>Get to know: </h4>
-        <ul>
-            <li><a href="#about">The Company</a></li>
-            <li><a href="#mission-vision">Our Mission and Vission</a></li>
-            <!--<li><a href="#contact">Contact Us</a></li>-->
-        </ul>
-    </div>    
+        <div class="widget-holder">
+            <div class="widget-body">
+                <h4>Get to know: </h4>
+                <ul>
+                    <li><a href="#about">The Company</a></li>
+                    <li><a href="#mission-vision">Our Mission and Vission</a></li>
+                    <li><a href="#contact-information">Contact Information</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    <div class="clearfix"></div>
 </div>
 @stop
 

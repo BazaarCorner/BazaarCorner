74cfb8044f911f8cd8bb37e1c8e3b4862d29d390
gabbydgab <gamba@gabbydgab.com>
2015-04-23 05:18:50 +0800

    updated files

--- app/Models/Catalog/Brand.php
+++ app/Models/Catalog/Brand.php
@@ -4,28 +4,15 @@ namespace BazaarCorner\Models\Catalog;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Brand extends Eloquent{
-
-	protected $table 		= 'brands';
-	protected $primaryKey 	= 'id';
-	protected $hidden 		= array(
-									'created_at'
-									,'updated_at'
-									,'is_active'
-								);
-	
-	public function getBrandList(){
-		return Brand::where('is_active',1)
-					->orderBy('name','ASC')
-					->get();
-	}
-	
-	/*====================================================================================================================================
-	| RELATIONSHIPS
-	/*====================================================================================================================================*/
-	public function categories()
-    {
-        return $this->belongsToMany('Category','brand_categories');
-    }
-
+class Brand extends Model
+{
+    protected $table = 'brands';
+    
+    protected $fillable = [
+        'name',
+        'slug',
+        'image',
+        'description',
+        'is_active',
+    ];    
 }
--- app/Models/Catalog/Category.php
+++ app/Models/Catalog/Category.php
@@ -1,47 +1,10 @@
 <?php
 
-class Category extends Eloquent{
+namespace BazaarCorner\Models\Catalog;
 
-	protected $table 		= 'categories';
-	protected $primaryKey 	= 'id';
-	protected $hidden 		= array(
-									'created_at'
-									,'updated_at'
-								);
+use Illuminate\Database\Eloquent\Model;
 
-	public function getCategories(){
-		$category_list = Category::selectRaw('id,name')
-						->where('is_active',1)
-						->orderBy('name','ASC')
-						->get();
-	}
-
-	public function getMainCategories(){
-		return Category::MainCategory()->orderBy('name','ASC')->get();
-	}
-
-	public function getSubCategories($id){
-		return Category::selectRaw('id,name')
-						->where('parent_id',$id)
-						->where('is_active',1)
-						->orderBy('name','ASC')
-						->get();
-	}
-
-	public function getCategoryDetails($id){
-		return Category::find($id);	
-	}
-
-	/*====================================================================================================================================
-	| RELATIONSHIPS
-	/*====================================================================================================================================*/
-	public function brands(){
-		return $this->belongsToMany('Brand','brand_categories');
-	}
-	/*====================================================================================================================================
-	| QUERY SCOPES
-	/*====================================================================================================================================*/
-	public function scopeMainCategory($query){
-		return $query->where('parent_id',null)->where('is_active',1);
-	}
+class Category extends Model
+{
+    protected $table = 'categories';
 }

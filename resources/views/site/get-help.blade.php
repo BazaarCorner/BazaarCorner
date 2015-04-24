e426a824c47a02b98065ed37d7f55890129f5f4b
gabbydgab <gamba@gabbydgab.com>
2015-04-24 15:05:24 +0800

    updated FAQ page

--- resources/views/site/get-help.blade.php
+++ resources/views/site/get-help.blade.php
@@ -69,12 +69,161 @@
             
         </ul>
     </section>
+    <section id="how-it-works">
+        <h3>How does Bazaar Corner work?</h3>
+        <p>
+            Bazaar Corner is an online market place where members can collect,
+            save, share and sell the products they like.  It’s a platform where
+            we connect merchants and consumers socially.  It brings a one of a
+            kind experience specially for the consumers on how they will greatly
+            enjoy the best deals, great offers and huge discounts with wide
+            array of available collectibles, fashion products and merchandise
+            they can choose from.
+        </p>
+        <p>Products here were curated by the international community.</p>
+    </section>
+    <section id="purchase">
+        <h3>Do I need to register before I can make a purchase?</h3>
+        <p>
+            If you are a guest shopper, no registration required. A “continue as
+            a guest” button which will take you  straight to checkout.
+        </p>
+    </section>
+    <section id="merchant">
+        <h3>How does Bazaar Corner works for merchants like me?</h3>
+        <p>
+            Bazaar Corner will help you advertise, get discovered,  share and
+            sell your products. Members go here to browse products they want and
+            shop.  Its all about fun and discovery, setting up our users in 
+            pre-shopping mode, making them more excited on what are those next
+            things to come.
+        </p>
+    </section>
+    <section id="shipping">
+        <h3>Where does Bazaar Corner ship?</h3>
+        <p>
+            Bazaar Corner ships worldwide, yet some items may only be available
+            for delivery to certain countries.
+        </p>
+    </section>
+    <section id="returned-items">
+        <h3>How do I return the item I’ve purchased?</h3>
+        <p>
+            To return an item for refund or replacement:            
+            <ul>
+                <li>
+                    Find the item in the “Order Management” tab in your account
+                    setting page.
+                </li>
+                <li>
+                    Select “Reason  for Return” and it all depends on the
+                    seller’s return policy preference. We may put you in touch
+                    with the seller.
+                </li>
+                <li>
+                    If the seller uses Bazaar Corner hassle-free returns or 
+                    chooses to use an Bazaar Corner-generated shipping label, we
+                    provide you with a return shipping label and packing slip. 
+                    If a shipping label isn't available for the item, we ask you
+                    to contact the seller about <strong> alternative shipping arrangements</strong>.
+                    The seller may also provide you their own shipping label.
+                </li>
+                <li>
+                    Right after that, print the label, pack the item and return
+                    it to seller not later than 5 days.
+                </li>
+            </ul>
+        </p>
+        <p>
+            <strong>Note:</strong> Before you request for a return item,  you must check seller’s
+            return policy for return periods and other information.
+        </p>
+        <p>
+            Items can be returned:
+            <ul>
+                <li>
+                    Merchandise that is being returned within the 30 day return
+                    period.
+                </li>
+                <li>
+                    Merchandise that has not been removed from its original
+                    packaging.
+                </li>
+                <li>Merchandise that has not been installed</li>
+            </ul>
+        </p>
+        <p>
+            Items that are non-returnable:
+            <ul>
+                <li>Merchandise with shrink wrap that is missing or torn</li>
+                <li>Merchandise that is used or shows signs of installation</li>
+                <li><strong>All special order merchandise</strong></li>
+                <li>Discontinued merchandise</li>
+                <li>Parts or replacement parts</li>
+                <li>Products $15 or less in value</li>
+            </ul>
+        </p>
+    </section>
+    <section id="order-status">
+        <h3>How to check the status of my order?</h3>
+        <p>
+            To view the status of your order you need to log-in into your Bazaar
+            Corner Account and visit  My Orders Management page to see the exact
+            status of the items that you have bought.
+        </p>
+    </section>
+    <section id="payment-method">
+        <h3>What payment methods can I use to pay for my order?</h3>
+        <p>
+            We  accept Visa, Mastercard, Amercian Exepress, Discover Card,
+            Paypal and Bitcon.
+        </p>
+    </section>
+    <section id="refund">
+        <h3>How long does it take for me to get the refund?</h3>
+        <p>It will take minimum of 7 days for you to get the refund.</p>
+        <p>
+            When the item isn’t as described we encourage our members to 
+            communicate with each other when there is a problem with the 
+            transaction.
+        </p>
+        <p>
+            <em>Sample computation:</em>
+            <strong>
+                *Total purchase price + **Original shipping cost = Total refund.
+            </strong>
+            <br><br>
+            * (Inclusive of the item cost, other charges and tax)
+            <br>
+            **(paid by the buyer)
+        </p>
+    </section>
+    <section id="cancel-order">
+        <h3>How to cancel a transaction?</h3>
+        <p>
+            When you need to cancel a transaction, you may need to contact the
+            buyer first and let them  know  that you’re canceling the
+            transaction. In this case you can cancel a transaction only 7 days
+            after the sale and even if the buyer has already paid for the item.
+        </p>
+        <p>
+            You wont be able to cancel  if the buyer has already opened/used the
+            item. Multiple items of purchase can only cancel the whole
+            transaction and not as per individual item that the buyer has bought.
+        </p>
+    </section>
+    
+    <section id="more-question">
+        <h3>MORE QUESTIONS?</h3>
+        <p>Please feel free to email us at help@bazaarcorner.com</p>
+    </section>    
 </div>
 <div class="col-md-3">
-    <div class="affix">
+    <div class="container-fluid">
         <div class="widget-holder">
-            <div class="widget-body">
-                <h4>Frequently Asked Questions:</h4>
+            <div class="widget-body">   
+                <h4>Frequently Asked Questions</h4>
+                <h5>General Questions:</h5>
                 <ul>
                     <li>
                         <a href="#information-collection">
@@ -85,8 +234,47 @@
                         <a href="#information-usage">
                             Usage of personal information
                         </a>
-                    </li>            
+                    </li>
                 </ul>
+                
+                <h5>Common Questions:</h5>
+                <ul>
+                    <li>
+                        <a href="#how-it-work">
+                            How does Bazaar Corner work?
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#purchase">
+                            Do I need to register before I can make purchase?
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#merchant">
+                            How does Bazaar Corner works for merchants like me?
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#shipping">
+                            Where does Bazaar Corner ship?
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#returned-items">
+                            How do I return the item I’ve purchased?
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#order-status">
+                            How to check the status of my order?
+                        </a>
+                    </li>
+                </ul>
+                <h5>
+                    <a href="#more-question">
+                        Want to ask question(s) personally?
+                    </a>
+                </h5>
             </div>
         </div>
     </div>

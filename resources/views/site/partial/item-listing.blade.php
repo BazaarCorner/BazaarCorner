a0d4225f5ea3586716d3dc3a1e121a31d0aa5778
gabbydgab <gamba@gabbydgab.com>
2015-04-28 19:24:01 +0800

    removed inactive pagination

--- resources/views/site/partial/item-listing.blade.php
+++ resources/views/site/partial/item-listing.blade.php
@@ -1,18 +1,5 @@
 <div class="container-fluid">
     <div class="content-body">
-        <div class="row-fluid">
-            <!-- add sorting filter here -->
-            <div class="col-md-12">
-                <nav class="pull-right">
-                    <ul class="pagination">
-                        <li><a href="#" class="page-active">1</a></li>
-                        <li><a href="#">2</a></li>
-                        <li><a href="#">3</a></li>
-                        <li><a href="#" aria-label="Next">Next</a></li>
-                    </ul>
-                </nav>
-            </div>
-        </div>
         <div class="divider"><hr><br></div>
         <div class="row-fluid">
             <div class="col-md-12">
@@ -20,18 +7,6 @@
                 <div class="clearfix"></div>
             </div>
         </div>
-        <div class="divider"><br><hr></div>
-        <div class="row-fluid">
-            <div class="col-md-12">
-                <nav class="pull-right">
-                    <ul class="pagination">
-                        <li><a href="#" class="page-active">1</a></li>
-                        <li><a href="#">2</a></li>
-                        <li><a href="#">3</a></li>
-                        <li><a href="#" aria-label="Next">Next</a></li>
-                    </ul>
-                </nav>
-            </div>
-        </div>
+        <!--<div class="divider"><br><hr></div>-->        
     </div>
 </div>

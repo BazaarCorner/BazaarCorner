36fb36a29163263b96e0265295609f8cdb631a55
gabbydgab <gamba@gabbydgab.com>
2015-07-07 07:19:06 +0800

    fixed image pulling

--- resources/views/membership/index.blade.php
+++ resources/views/membership/index.blade.php
@@ -1,3 +1,4 @@
+{{dd($member['info'])}}
 @extends('layout.site')
 @section('content')
 <div class="col-md-3">
@@ -15,7 +16,7 @@
     <div class="row-fluid" style="background-image: url('assets/bazaarcorner/img/site/cover.jpg'); background-repeat: no-repeat; background-position: center; max-height: 135px; width:100%; background-size: cover; display: block;">
         <div class="merchant-logo" style="display: block; text-align-last: inherit; padding-top: 35px; padding-left: 10px; background: ">
             <img class="img-center img-responsive img-circle" 
-                                src="{{ isset($merchant->logo) ? url(env('CDN').$product->display_image) : asset('assets/bazaarcorner/img/no-image.gif') }}"
+                                src="{{ isset($member['info']['logo']) ? url(env('MERCHANT_LOGO').$member['info']['logo']) : asset('assets/bazaarcorner/img/no-image.gif') }}"
                                 style="width: 100px; height: 100px;">
         </div>
         <div class="merchant-description" style="background:#cacaca; height: inherit; padding: 10px 0; color: white;">
